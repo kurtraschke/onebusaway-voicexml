9a30d13e54fc6b6972b9af0e40111b4e4bdda716
Kurt Raschke <kurt@kurtraschke.com>
2013-04-30 21:21:29 -0400

    Initial commit; not entirely functional.

---
+++ src/main/java/org/onebusaway/voicexml/actions/StopAction.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 OneBusAway.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.voicexml.actions;
+
+import java.util.List;
+import org.onebusaway.nyc.presentation.model.SearchResult;
+import org.onebusaway.nyc.presentation.service.realtime.RealtimeService;
+import org.onebusaway.nyc.presentation.service.search.SearchResultFactory;
+import org.onebusaway.nyc.transit_data.services.NycTransitDataService;
+import org.onebusaway.nyc.util.configuration.ConfigurationService;
+import org.onebusaway.presentation.impl.NextActionSupport;
+import org.onebusaway.transit_data.model.SearchQueryBean;
+import org.onebusaway.transit_data.model.StopBean;
+import org.onebusaway.voicexml.model.StopResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author kurt
+ */
+public class StopAction extends NextActionSupport {
+
+    @Autowired
+    private ConfigurationService _configurationService;
+    @Autowired
+    private NycTransitDataService _nycTransitDataService;
+    @Autowired
+    private RealtimeService _realtimeService;
+    private String _stopId;
+    private StopResult _stopResult;
+
+    public void setStopId(String stopId) {
+        this._stopId = stopId;
+    }
+
+    public String getStopId() {
+        return _stopId;
+    }
+    
+    public StopResult getStopResult() {
+        return _stopResult;
+    }
+
+    public String execute() {
+        StopBean stop = _nycTransitDataService.getStop(_stopId);
+
+        SearchResultFactory factory = new SearchResultFactoryImpl(
+                _nycTransitDataService, _realtimeService, _configurationService);
+
+        _stopResult = (StopResult)factory.getStopResult(stop, null);
+    
+        return SUCCESS;
+    }
+}
---
+++ src/main/java/org/onebusaway/voicexml/actions/StopSearchAction.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013 OneBusAway.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.voicexml.actions;
+
+import com.google.common.collect.Iterables;
+import java.util.List;
+import org.onebusaway.nyc.presentation.model.SearchResult;
+import org.onebusaway.nyc.presentation.service.realtime.RealtimeService;
+import org.onebusaway.nyc.presentation.service.search.SearchResultFactory;
+import org.onebusaway.nyc.transit_data.services.NycTransitDataService;
+import org.onebusaway.nyc.util.configuration.ConfigurationService;
+import org.onebusaway.presentation.impl.NextActionSupport;
+import org.onebusaway.transit_data.model.SearchQueryBean;
+import org.onebusaway.transit_data.model.StopBean;
+import org.onebusaway.voicexml.model.StopResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author kurt
+ */
+public class StopSearchAction extends NextActionSupport {
+
+    @Autowired
+    private ConfigurationService _configurationService;
+    @Autowired
+    private NycTransitDataService _nycTransitDataService;
+    private String _stopCode;
+    private String _stopId;
+    private List<StopBean> _matches;
+
+    public void setStopCode(String stopCode) {
+        _stopCode = stopCode;
+    }
+
+    public String getStopCode() {
+        return _stopCode;
+    }
+
+    public List<StopBean> getMatches() {
+        return _matches;
+    }
+    
+    public String getStopId() {
+        return _stopId;
+    }
+
+    private SearchQueryBean makeSearchQuery(String query) {
+        SearchQueryBean sqb = new SearchQueryBean();
+        sqb.setQuery(query);
+        sqb.setMinScoreToKeep(1.0);
+        sqb.setMaxCount(9);
+        return sqb;
+    }
+
+    @Override
+    public String execute() {
+        _matches = _nycTransitDataService.getStops(makeSearchQuery(_stopCode)).getStops();
+        
+        if (_matches.isEmpty()) {
+            return "showNotFound";
+        } else if (_matches.size() == 1) {
+            _stopId = Iterables.getOnlyElement(_matches).getId();
+            return "showStopResult";
+        } else {
+            return SUCCESS;
+        }
+    }
+}
